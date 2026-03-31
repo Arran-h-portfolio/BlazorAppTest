e1222e38dc7a4e9ebd94a8f66b27b955c7c382eb
Arran Hewitson <AAAHStudios@outlook.com>
2026-03-31 07:54:24 +0100

    Updated staff manager page and added an IsActive input into StaffInput.cs

--- Components/Models/StaffInput.cs
+++ Components/Models/StaffInput.cs
@@ -4,6 +4,8 @@ namespace StaffInput.Models
 {
     public class StaffInput
     {
+        public required string EmployeeID { get; set; } //Unique identifier for each employee, can be used to link to other tables in the future if needed
+
         public required string FirstName { get; set; }
         
         public required string LastNameInitial { get; set; }
@@ -15,5 +17,7 @@ namespace StaffInput.Models
         public required string StartTime { get; set; }
 
         public required string EndTime { get; set; }
+
+        public required bool IsActive { get; set; }
     }
 }
