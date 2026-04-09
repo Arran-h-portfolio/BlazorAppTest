fa0032a1d68184e515395a0244ddf1b9970d5786
Arran Hewitson <AAAHStudios@outlook.com>
2026-04-09 22:27:25 +0100

    general assembly info update

--- obj/Debug/net10.0/BlazorAppTest.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorAppTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111c2ff2576720e8390674bdca574c20dc2b1e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e9da5a21c845e1f1fcf63c9453eb0a40f08890")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
